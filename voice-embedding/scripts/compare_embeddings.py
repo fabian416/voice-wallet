aeb86b4fdab94d8f5640388344880139b2899dbb
fabiandiaz416 <diazfabian99@icloud.com>
2025-04-28 20:03:16 -0300

    set threshold for proof of concept into 0.675

--- voice-embedding/scripts/compare_embeddings.py
+++ voice-embedding/scripts/compare_embeddings.py
@@ -3,13 +3,13 @@ import numpy as np
 
 EMBEDDING_DIR = "./embeddings"
 
-embedding1 = np.load(os.path.join(EMBEDDING_DIR, "audio1.npy"))
-embedding2 = np.load(os.path.join(EMBEDDING_DIR, "audio2.npy"))
+embedding1 = np.load(os.path.join(EMBEDDING_DIR, "audio8.npy"))
+embedding2 = np.load(os.path.join(EMBEDDING_DIR, "audio4.npy"))
 
 distance = np.linalg.norm(embedding1 - embedding2)
 print(f"Distance: {distance}")
 
-threshold = 0.25
+threshold = 0.675
 
 if distance < threshold:
     print("Voices match")
