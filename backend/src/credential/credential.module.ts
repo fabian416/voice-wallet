15e229d177517b7f30f226facdfcf14e9340e194
S1X3l4 <alexis.janko@gmail.com>
2025-05-15 13:18:12 -0300

    conectando credentialGuard

--- backend/src/credential/credential.module.ts
+++ backend/src/credential/credential.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { CredentialService } from './credential.service';
 import { CredentialController } from './credential.controller';
+import { CredentialGuard } from './guards/credential.guard';
 
 @Module({
     imports: [],
-    providers: [CredentialService],
+    providers: [CredentialService, CredentialGuard],
     controllers: [CredentialController],
-    exports: [CredentialService],
+    exports: [CredentialService, CredentialGuard],
 
 })
 export class CredentialModule {}
