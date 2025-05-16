12d1ac36d1d5fb37457910e46a3478fb1790094b
lucholeonel <carrenoluciano@gmail.com>
2025-05-16 19:02:05 -0300

    setup docker

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,9 +6,9 @@ async function bootstrap() {
   dotenv.config();
   const app = await NestFactory.create(AppModule);
     app.enableCors({
-    origins: ['http://localhost:5173', 'http://localhost:3000',],
+    origins: ['*'],
     credentials: true,
   });
-  await app.listen(process.env.PORT || 5000);
+  await app.listen(process.env.PORT || 5001);
 }
 bootstrap();
